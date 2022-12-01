c60eef95eb6c7044e4a2d14707d9db3dc7d75a6e
RAZVAN TRUFIN <razvantrufin99x@gmail.com>
2022-12-01 23:56:16 +0200

    Add project files.

---
+++ performance/PerformanceManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+
+namespace performance
+{
+
+      
+
+    public  class PerformanceManager
+    {
+        static PerformanceCounter cpuCounter;
+        static PerformanceCounter ramCounter;
+
+        static PerformanceManager()
+        {
+            cpuCounter = new PerformanceCounter();
+
+            cpuCounter.CategoryName = "Processor";
+            cpuCounter.CounterName = "% Processor Time";
+            cpuCounter.InstanceName = "_Total";
+
+            ramCounter = new PerformanceCounter("Memory", "Available MBytes");
+        }
+
+        public  string getCurrentCpuUsage()
+        {
+            return cpuCounter.NextValue() + "%";
+        }
+
+        public  string getAvailableRAM()
+        {
+            return ramCounter.NextValue() + "MB";
+        }
+    }
+}
